90514d5c7bc3cd35a868b7437d114b659f498549
CalvinMetheuver <20195680@student.hhs.nl>
2021-05-13 00:54:26 +0200

    GUI Loginscherm gemaakt

---
+++ src/main/java/controller/HUBController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class HUBController implements Initializable {
+
+    @FXML
+    private AnchorPane rootPane;
+
+    public void loguit(MouseEvent mouseEvent) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/HomeScreen.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+
+}
--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -4,10 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import model.Database;
 
-import java.awt.*;
+import javafx.scene.control.TextField;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -15,13 +18,37 @@ import java.util.ResourceBundle;
 
 
 public class LoginController implements Initializable {
+    Database db = Database.getInstance();
+
 
     @FXML
     private AnchorPane rootPane;
 
+    @FXML
+    private PasswordField wachtwoord;
+
+    @FXML
+    private TextField gebruikersnaam;
+
+    @FXML
+    private Label onvolledig;
+
+    @FXML
+    private Label onjuist;
+
+
     public void login(MouseEvent mouseEvent) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/HomeScreen.fxml"));
-        rootPane.getChildren().setAll(pane);
+        onvolledig.setVisible(false);
+        onjuist.setVisible(false);
+
+        if(gebruikersnaam.getText().equals("") || wachtwoord.getText().equals("")){
+            onvolledig.setVisible(true);
+        }else if (db.loginCheck(gebruikersnaam.getText(), wachtwoord.getText()) != null) {
+                AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/HUB.fxml"));
+                rootPane.getChildren().setAll(pane);
+            } else {
+            onjuist.setVisible(true);
+        }
     }
 
     @Override
